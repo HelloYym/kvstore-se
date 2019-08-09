9b8e11d6bcea8862e8703ab97c559d5cb6986de2
cyif <chenyifan3x@gmail.com>
2019-08-09 13:20:10 +0800

    socket client

--- src/include/utils.h
+++ src/include/utils.h
@@ -3,7 +3,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 #include "easylogging++.h"
 #include "params.h"
-#include "sys/socket.h"
 #include <cassert>
 
 #define KV_LOG(level) LOG(level) << "[" << __FUNCTION__ << ":" << __LINE__ << "] "
@@ -17,11 +16,10 @@ const uint8_t KV_OP_RECOVER = 5;
 #define KV_OP_SUCCESS "S"
 #define KV_OP_FAILED "F"
 
+const int PACKET_HEADER_SIZE = sizeof(int8_t) + sizeof(u_int32_t);
+const int MAX_PACKET_SIZE = PACKET_HEADER_SIZE + KEY_SIZE + VALUE_SIZE;
 #pragma pack(push)
 #pragma pack(1)
-// const int PACKET_HEADER_SIZE = sizeof(int32_t) * 4;
-const int PACKET_HEADER_SIZE = sizeof(int8_t);
-const int MAX_PACKET_SIZE = PACKET_HEADER_SIZE + KEY_SIZE + VALUE_SIZE;
 struct Packet {
     int len = 0;
     uint8_t type= 0;
@@ -29,26 +27,5 @@ struct Packet {
 };
 #pragma pack(pop)
 
-void recvPack(int fd, char * buf) {
-    auto bytes = recv(fd, buf, MAX_PACKET_SIZE, 0);
-    if (bytes == -1) {
-        printf("recv error\n"); return;
-    }
-    while (bytes < sizeof(int)) {
-        auto b = recv(fd, buf, MAX_PACKET_SIZE, 0);
-        if (b == -1) {
-            printf("recv error\n"); return;
-        }
-        bytes += b;
-    }
-    int total = *(int *) buf;
-    while (total != bytes) {
-        auto b = recv(fd, buf + bytes, MAX_PACKET_SIZE, 0);
-        if (bytes == -1) {
-            printf("recv error\n"); return;
-        }
-        bytes += b;
-    }
-}
 //////////////////////////////////////////////////////////////////////////////////////////////////
 #endif
